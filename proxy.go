06befaeecccd56746771aef0a13c345726801937
Filipe Peixoto <filipenos@gmail.com>
2014-11-04 11:55:59 +0000

    Create a handler to reverse proxy.

---
+++ proxy.go
@@ -0,0 +1,24 @@
+package proxy
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"appengine"
+	"appengine/urlfetch"
+)
+
+func init() {
+	http.HandleFunc("/", serve)
+}
+
+func serve(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	url := &url.URL{Path: "http://localhost:8888"}
+
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	proxy.Transport = &urlfetch.Transport{Context: c}
+	proxy.ServeHTTP(w, r)
+}
